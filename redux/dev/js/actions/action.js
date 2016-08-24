d6138f6293c7cf2c3902dbcd714901fa031a9239
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2016-08-24 12:25:42 +0530

    Redux with ReactJS
    This repo contains the lessions by YouTube channel "TheNewBoston".

---
+++ redux/dev/js/actions/action.js
@@ -0,0 +1,8 @@
+export const selectUser = (user)  => {
+	console.log(user.first);	
+	return {
+		type: 'USER_SELECTED',
+		payload: user
+	}
+};
+
---
+++ redux/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        inline: true,
+        contentBase: './src',
+        port: 3000
+    },
+    devtool: 'cheap-module-eval-source-map',
+    entry: './dev/js/index.js',
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    output: {
+        path: 'src',
+        filename: 'js/bundle.min.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin()
+    ]
+};
